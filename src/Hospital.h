bcea9c87e7dc6e5c92ee506e908cfd3c995ff892
Jason S <jschinis@gmail.com>
2024-11-16 09:04:55 -0500

    Started adding possible characteristics to hospital class.

--- src/Hospital.h
+++ src/Hospital.h
@@ -5,9 +5,25 @@
 #ifndef HOSPITAL_H
 #define HOSPITAL_H
 
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <string>
+using namespace std;
 
 
 class Hospital {
+    string hospital_pk;
+    string name;
+    string zip;
+    string subtype;
+    pair<int, int> geoCoords;
+    map<string, vector<double>> ordered_weekly_data;
+    unordered_map<string, vector<double>> unordered_weekly_data;
+
+public:
+    // Constructors
+    Hospital();
 
 };
 
