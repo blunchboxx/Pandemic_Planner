75d4a52150b43c9c9940932c062d36e2412f7860
user <user@user.com>
2024-11-30 17:11:50 -0500

    update .js and .html to only display hotspots if the user selects a state AND date for filtering

--- visuals/js/home.js
+++ visuals/js/home.js
@@ -1,19 +1,17 @@
 // displays the correct state selected
-function img_state_display() {
-    // get the user's month, year, and state selection
-    var month = document.getElementById("month").value;
-    var year = document.getElementById("year").value;
-    var state_name = document.getElementById("state").value;
+function img_state_display(month, year, state_name) {
 
-
+    // get the picture for the selected state and display it
     document.getElementById("image_section").style.backgroundImage = "url(img/" + state_name + ".png)";
 
+    // account for underscore in state names
     if (state_name.includes("_"))
     {
         state_name = state_name.replace("_", " ");
     }
 
-    document.getElementById("hot_words").innerText =  "Top 10 Hotspots for " + state_name + " in " + month + " " + year;
+    // update the heading for the user's selection 
+    document.getElementById("hot_words").innerText = "Top 10 Hotspots for " + state_name + " in " + month + " " + year;
 }
 
 // resets the state image to the default
@@ -33,55 +31,16 @@ function complete_reset() {
     document.querySelector('#hot_holder').style.display = 'none';
 }
 
-
-// get the data from our file based on the date only
-function get_data_date() {
-
-    // reset the state selection
-    img_state_reset();
-
-    // get the user's month and year selection
-    var month = document.getElementById("month").value;
-    var year = document.getElementById("year").value;
-
-    // update the hotspot words
-    document.getElementById("hot_words").innerText = "Top 10 Hotspots for " + month + " " + year;
-
-
-    // collect the placeholders for our file to write over
-    var first = document.getElementById("one").value;
-    var second = document.getElementById("two").value;
-    var third = document.getElementById("three").value;
-    var fourth = document.getElementById("four").value;
-    var fifth = document.getElementById("five").value;
-    var sixth = document.getElementById("six").value;
-    var seventh = document.getElementById("seven").value;
-    var eighth = document.getElementById("eight").value;
-    var ninth = document.getElementById("nine").value;
-    var tenth = document.getElementById("ten").value;
-
-
-    // call whatever function we use to get the data we want
-
-
-    // replace previous variables with new values
-
-
-    // display the new data
-    show_hot(first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth);
-}
-
 // get the data from our file based on the date and filter by state
 function get_data_state() {
 
-    // display the selected state
-    img_state_display();
-
-    // get the user's month, year, and state selection
+    // get the user's selections
     var month = document.getElementById("month").value;
     var year = document.getElementById("year").value;
     var state_name = document.getElementById("state").value;
 
+    // display the selected state based on user's input
+    img_state_display(month, year, state_name);
 
     // create the placeholders for our file to write over
     var first = document.getElementById("one").value;
